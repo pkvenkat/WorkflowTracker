dbfc6a0240248f216ec5b925a8b890ce35c2b104
minglu <luming0625@gmail.com>
2013-04-01 15:28:43 -0400

    add return Data[]

--- org.cishell.reference.gui.workflow/src/org/cishell/reference/gui/workflow/controller/WorkflowManager.java
+++ org.cishell.reference.gui.workflow/src/org/cishell/reference/gui/workflow/controller/WorkflowManager.java
@@ -2,20 +2,27 @@ package org.cishell.reference.gui.workflow.controller;
 
 import java.util.LinkedHashMap;
 
+import org.cishell.app.service.datamanager.DataManagerService;
+import org.cishell.framework.data.Data;
+import org.cishell.framework.data.DataProperty;
+import org.cishell.reference.gui.workflow.Activator;
 import org.cishell.reference.gui.workflow.Utilities.Constant;
 import org.cishell.reference.gui.workflow.model.Workflow;
 import org.cishell.reference.gui.workflow.model.NormalWorkflow;
+import org.osgi.framework.BundleContext;
 
 public class WorkflowManager {
 	static  private WorkflowManager manager = null; 
 	private LinkedHashMap<Long, Workflow> map ;
 	private Workflow currentWorkflow;
 	private Long lastCreatedID;
+	private static BundleContext bundleContext;
 	
    private	WorkflowManager()
    {
 	   map = new LinkedHashMap<Long, Workflow>();
 	   lastCreatedID = new Long(1);
+	   bundleContext = Activator.getContext();
    }
   static public WorkflowManager getInstance()
   {
@@ -37,6 +44,19 @@ public class WorkflowManager {
        }
 	  return currentWorkflow;
   }
+  
+  
+  public boolean runWorkflow(){
+	  DataManagerService dataManager = (DataManagerService)
+				bundleContext.getService(
+						bundleContext.getServiceReference(
+								DataManagerService.class.getName()));
+	  Data[] data = dataManager.getSelectedData();
+	  System.out.println(data[0].getMetadata().get(DataProperty.LABEL));
+	  
+	  return true;
+  }
+  
   
 public Long getUniqueInternalId()
   {
